0a9a249b09212ceaed8a6cddce7d743b51fa313a
Thamindu Chankana Gamage <it20604530@my.sliit.lk>
2022-02-20 17:55:41 +0530

    Added my details to the top

--- IT20604530.java
+++ IT20604530.java
@@ -1,3 +1,10 @@
+/*
+ID :- IT20604530
+Name :- Thamindu Chankana Menuwara Gamage
+Y2.S1 01.2
+Malabe
+*/
+
 import java.util.*;//Import java utilization package
 import java.util.Random;// Import Random util to generate random values
 
